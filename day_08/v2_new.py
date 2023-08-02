9dfb3e8a09f699bcc830826a99fd3ca0b4598f63
Asmita Pramanik <asmitap2@illinois.edu>
2023-08-02 14:37:43 -0400

    atmosphere_v3

--- day_08/v2_new.py
+++ day_08/v2_new.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Wed Aug  2 14:17:45 2023
+
+@author: anup
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 
 
 __author__ = "Asmita Pramanik"
@@ -18,18 +26,16 @@ from tridiagonal import solve_tridiagonal
 
 if __name__ == "__main__":
 
-    dx = 4.0 # km
+    dx = 0.25
 
     # set x with 1 ghost cell on both sides:
-        
-    x = 100 + np.arange(-dx, 400 + 2 * dx, dx)
+    x = np.arange(-dx, 10 + 2 * dx, dx)
 
-    # t_lower = 200.0
+    t_lower = 200.0
     
     # t_upper = 1000.0, we commented this out to check how our plots change with changing variables
 
     nPts = len(x)  #given
-    
     
     # set default coefficients for the solver:
     a = np.zeros(nPts) - 1
@@ -39,16 +45,16 @@ if __name__ == "__main__":
     
     
     # lambda value:
-    lam = 80
+    lam = 10
     
     #defining sun heat
-    sun_heat = 0.4
+    sun_heat = 100
     
     #initializing Q "heat"
     Q = np.zeros(nPts)
     
     
-    Q[(x>200) & (x<400)] = sun_heat
+    Q[(x>3) & (x<7)] = sun_heat
     
     
     dz = x[1] - x[0]  #determining dz
@@ -56,7 +62,7 @@ if __name__ == "__main__":
     
  # ////////////////////// this portion of code works on time dependence /////////////////////////////
     
-    nDays = 27    #number of days
+    nDays = 3    #number of days
     local_time = 0  #local time
     lon = 100        #longitude (can be changed to analyze plots)
     dt = 1        # time given in hours
@@ -64,35 +70,20 @@ if __name__ == "__main__":
     
     temp= [] #initialising array of times to generate contour plot
     
-# ////////////// the following variables are initialized to indicate presence of tides /////////////////////
     
-    AmpDi = 10
-    AmpSd = 5
-    PhaseDi = np.pi/2
-    PhaseSd = (3 * np.pi) / 2
-    
-    # tidal components
-    AmpDi_SinFunction = AmpDi * np.sin(((local_time/24)*2*np.pi) + PhaseDi)
-    AmpSd_SinFunction = AmpSd * np.sin(((local_time/24)* 2 * 2 * np.pi) + PhaseDi)
-    
-     
     # we put the following two lines before the for loop to generate a single image instead of several images
     fig = plt.figure(figsize = (10,10))
     ax = fig.add_subplot(111)
     
-    f_107 = 100 + (50/(24*365))*times + 25*np.sin(2*np.pi*(times/(27*24)))
     
-    for hour in range(len(times)):
-        
-        sun_heat = f_107[hour] * (0.4/100)
-        t_lower = 200  +  AmpDi_SinFunction +  AmpSd_SinFunction
+    for hour in times:
         ut = hour % 24
         local_time = lon/15 + ut
-        factor = - np.cos(local_time * 2 * np.pi / 24)
+        factor = -np.cos(local_time * 2*np.pi / 24)
         if factor < 0:
             factor = 0
         Q_euv = np.zeros(nPts) #EUV : Extreme Ultra-Violet 
-        Q_euv[(x > 200)&(x < 400)] = sun_heat * factor
+        Q_euv[(x>3)&(x<7)] = sun_heat * factor
         
         d = (Q + Q_euv)*(dz**2)/lam
         
